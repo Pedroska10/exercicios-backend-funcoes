1cf31f14b365c8886037b2780c89875fb9fc39d0
Pedro Henrique <125500440+Pedroska10@users.noreply.github.com>
2024-01-21 17:27:06 -0300

    Add files via upload

---
+++ 01/index.js
@@ -0,0 +1,55 @@
+const prova = {
+  aluno: "João",
+  materia: "Português",
+  valor: 10,
+  questoes: [
+    {
+      resposta: "a",
+      correta: "b",
+    },
+    {
+      resposta: "c",
+      correta: "b",
+    },
+    {
+      resposta: "e",
+      correta: "b",
+    },
+    {
+      resposta: "b",
+      correta: "c",
+    },
+    {
+      resposta: "a",
+      correta: "c",
+    },
+  ],
+};
+
+const corrigirProva = (prova) => {
+  let questoesCorretas = 0;
+
+  for (const questao of prova.questoes) {
+    if (questao.resposta === questao.correta) {
+      questoesCorretas++;
+    }
+  }
+
+  let valorPorQuestao = prova.valor / prova.questoes.length;
+
+  let notaDaProva = valorPorQuestao * questoesCorretas;
+
+  if (questoesCorretas === 0) {
+    console.log(`${prova.aluno} não acertou qualquer questão e obteve nota 0`);
+  } else if (questoesCorretas === 1) {
+    console.log(
+      `O aluno(a) ${prova.aluno} acertou 1 questão e obteve nota ${notaDaProva}`
+    );
+  } else {
+    console.log(
+      `O aluno(a) ${prova.aluno} acertou ${questoesCorretas} questões e obteve nota ${notaDaProva}`
+    );
+  }
+};
+
+corrigirProva(prova);
